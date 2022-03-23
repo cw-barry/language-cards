fd7445116db535c18cd2a414866262bc01b5611f
Barry Mitchell <barry@clarusway.com>
2022-03-24 00:45:07 +0300

    add language cards app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import reactImg from './assets/react.svg';
+import Card from './components/Card';
+import { categories as data } from './backend/data';
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={reactImg} alt="react logo" className="top-img" />
+        <section className="menu-container">
+          <div className="bars"></div>
+          <section className="top"></section>
+          <section className="bottom">
+            {/* <Card {...data[0]} />
+            <Card {...data[1]} />
+            <Card {...data[2]} /> */}
+            {data.map((item, index) => (
+              <Card key={index} {...item} />
+            ))}
+          </section>
+        </section>
+      </div>
     </div>
   );
 }
